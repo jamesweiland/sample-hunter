7fd9a975218f06fb8113f65a5e26ac85390bb02c
James Weiland <jamesweiland04@gmail.com>
2025-06-09 10:25:26 -0400

    fix songpairsdataset to truncate/pad if necessary

--- sample_hunter/pipeline/song_pairs_dataset.py
+++ sample_hunter/pipeline/song_pairs_dataset.py
@@ -13,13 +13,9 @@ from sample_hunter._util import (
     HOP_LENGTH,
     N_MELS,
     plot_spectrogram,
-    SNIPPET_LENGTH,
     WINDOW_SIZE,
-    STEP_SIZE,
     DEVICE,
 )
-
-NUM_SAMPLES = SNIPPET_LENGTH * SAMPLE_RATE
 
 
 class SongPairsDataset(Dataset):
@@ -37,8 +33,7 @@ class SongPairsDataset(Dataset):
         annotations_file: Path,
         mel_spectrogram: MelSpectrogram,
         target_sample_rate: int,
-        window_size: int,
-        step_size: int,
+        num_samples: int,
         device: str,
     ):
         """Initialize a SongPairs dataset. Each pair consists of an original audio
@@ -49,8 +44,7 @@ class SongPairsDataset(Dataset):
         self.audio_dir = audio_dir
         self.mel_spectrogram = mel_spectrogram.to(device)
         self.target_sample_rate = target_sample_rate
-        self.window_size = window_size
-        self.step_size = step_size
+        self.num_samples = num_samples
         self.device = device
 
         if "song_id" not in self.annotations.columns:
@@ -85,18 +79,18 @@ class SongPairsDataset(Dataset):
         unobfuscated_signal = self._resample(unobfuscated_sr, unobfuscated_signal)
         obfuscated_signal = self._resample(obfuscated_sr, obfuscated_signal)
 
-        # make windows
-        unob_windows = self._unfold(unobfuscated_signal)
-        ob_windows = self._unfold(obfuscated_signal)
+        # truncate/pad signals if necessary
+        unobfuscated_signal = self._pad_or_truncate(unobfuscated_signal)
+        obfuscated_signal = self._pad_or_truncate(obfuscated_signal)
 
         # apply the transformation (spectrogram) to each window
-        unob_specs = torch.stack([self.mel_spectrogram(w) for w in unob_windows], dim=0)
-        ob_specs = torch.stack([self.mel_spectrogram(w) for w in ob_windows], dim=0)
+        unob_spec = self.mel_spectrogram(unobfuscated_signal)  # anchor
+        ob_spec = self.mel_spectrogram(obfuscated_signal)  # positive
 
         # finally, get the song id associated with this snippet
         song_id = self.annotations.at[idx, "song_id"]
 
-        return unob_specs, ob_specs, song_id
+        return unob_spec, ob_spec, song_id
 
     def _get_unobfuscated_path(self, idx):
         return self.annotations.at[idx, "unob_seg"]
@@ -104,14 +98,14 @@ class SongPairsDataset(Dataset):
     def _get_obfuscated_path(self, idx):
         return self.annotations.at[idx, "ob_seg"]
 
-    def _mix_down(self, signal: Tensor):
+    def _mix_down(self, signal: Tensor) -> Tensor:
         """If the signal is more than one channel, mix down to mono"""
         if signal.shape[0] == 1:
             return signal
 
         return torch.mean(signal, dim=0, keepdim=True)
 
-    def _resample(self, original_sampling_rate: int, signal: Tensor):
+    def _resample(self, original_sampling_rate: int, signal: Tensor) -> Tensor:
         """If the signal has a different sampling rate than the target sample rate, downsample it"""
         if original_sampling_rate == self.target_sample_rate:
             return signal
@@ -121,13 +115,15 @@ class SongPairsDataset(Dataset):
         ).to(self.device)
         return resampler(signal)
 
-    def _unfold(self, signal: Tensor):
-        """Split the signal into many windows"""
-        windows = signal.unfold(
-            dimension=1, size=self.window_size, step=self.step_size
-        ).permute(1, 0, 2)
-        print(windows.shape)
-        return windows
+    def _pad_or_truncate(self, signal: Tensor) -> Tensor:
+        if signal.shape[1] > self.num_samples:
+            # truncate
+            return signal[:, : self.num_samples]
+        if signal.shape[1] < self.num_samples:
+            # pad
+            num_missing_samples = self.num_samples - signal.shape[1]
+            return torch.nn.functional.pad(signal, (0, num_missing_samples))
+        return signal
 
 
 if __name__ == "__main__":
@@ -139,11 +135,12 @@ if __name__ == "__main__":
 
     dataset = SongPairsDataset(
         audio_dir=Path("/home/james/code/sample-hunter/_data/audio-dir/"),
-        annotations_file=Path("/home/james/code/sample-hunter/_data/annotations.csv"),
+        annotations_file=Path(
+            "/home/james/code/sample-hunter/_data/new_annotations.csv"
+        ),
         mel_spectrogram=mel_spectrogram,
         target_sample_rate=SAMPLE_RATE,
-        window_size=WINDOW_SIZE,
-        step_size=STEP_SIZE,
+        num_samples=WINDOW_SIZE,
         device=DEVICE,
     )
 
@@ -154,9 +151,5 @@ if __name__ == "__main__":
     print(type(id))
 
     print(X.shape, Y.shape)
-    for x in X:
-        print(x.shape)
-        plot_spectrogram(x, "unobfuscated")
-    for y in Y:
-        print(y.shape)
-        plot_spectrogram(y, "obfuscated")
+    plot_spectrogram(X, "unobfuscated")
+    plot_spectrogram(Y, "obfuscated")
