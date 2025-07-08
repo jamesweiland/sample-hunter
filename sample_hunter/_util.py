ef4ec6662b33c089f5c6d2f5dfb0738dea40e143
James Weiland <jamesweiland04@gmail.com>
2025-07-08 15:27:05 -0500

    add play_tensor_audio for debugging

--- sample_hunter/_util.py
+++ sample_hunter/_util.py
@@ -10,6 +10,7 @@ from torch import Tensor
 import matplotlib.pyplot as plt
 from typing import Any
 from abc import ABC, abstractmethod
+import sounddevice as sd
 
 import pandas as pd
 
@@ -61,6 +62,20 @@ def plot_spectrogram(tensor: Tensor, title: str = "Spectrogram"):
     plt.ylabel("Mel Frequency Bin")
     plt.xlabel("Time Frame")
     plt.show()
+
+
+def play_tensor_audio(tensor: torch.Tensor, sample_rate=config.preprocess.sample_rate):
+    """Halt script execution to play a tensor as audio"""
+    # tensor: shape (1, T) or (T,)
+    arr = tensor.cpu().numpy()
+    if arr.ndim == 2:
+        if arr.shape[0] == 2:  # stereo
+            arr = arr.T
+        elif arr.shape[0] == 1:  # mono
+            arr = arr.squeeze(0)  # sd doesn't like the extra dimension
+
+    sd.play(arr, samplerate=sample_rate)
+    sd.wait()  # Wait until playback is finished
 
 
 def save_to_json_or_csv(path: Path, df: pd.DataFrame) -> None:
