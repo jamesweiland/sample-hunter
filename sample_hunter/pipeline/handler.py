37d70ec1466136fbc34d948c1fb120787b5f5433
James Weiland <jamesweiland04@gmail.com>
2025-07-23 14:28:10 -0500

    hf endpoint handler

--- sample_hunter/pipeline/handler.py
+++ sample_hunter/pipeline/handler.py
@@ -6,8 +6,17 @@ When somebody tries to deploy the model hosted on HF, this is what executes.
 Accepts a data dictionary with field `inputs` that are a list of 1 or more
 torch.Tensors, and returns the corresponding embeddings.
 """
-import yaml
+
+import torch
+from pathlib import Path
+from sample_hunter._util import load_model
+
 
 class EndpointHandler:
     def __init__(self, model_dir, **kwargs):
-        with yaml.safe_load
+        self.model = load_model(Path("./7-18-2025-1.pth"))
+        self.model.eval()
+
+    def __call__(self, data):
+        with torch.no_grad():
+            return self.model(data)
